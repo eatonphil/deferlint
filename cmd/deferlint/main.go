97714863d48be7f0856489856ae15e19fdb613f9
Phil Eaton <philneaton95@gmail.com>
2019-07-16 09:18:38 -0400

    Names line up

--- cmd/deferlint/main.go
+++ cmd/deferlint/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"golang.org/x/tools/go/analysis/singlechecker"
 
-	"github.com/eatonphil/deferlinter"
+	"github.com/eatonphil/deferlint"
 )
 
 func main() {
-	singlechecker.Main(deferlinter.Analyzer)
+	singlechecker.Main(deferlint.Analyzer)
 }
